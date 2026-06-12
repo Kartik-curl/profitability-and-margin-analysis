0984a28f065392e3272e755dc24f1565322b6aad
kartik <shyamlalrajput122@gmail.com>
2026-06-12 14:47:58 +0530

    cache deleted

---
+++ app.py
@@ -0,0 +1,147 @@
+from narwhals import col
+import numpy as np
+from utils import calculate_kpis, cost_vs_sales, div_margin_chart, div_sales_chart, factory_analysis, filter_data, load_data, margin_risk, profit_pareto, profit_pareto_chart, revenue_pareto, revenue_pareto_chart, top_products_by_margin, top_products_by_profit
+import streamlit as st
+st.set_page_config(layout="wide",page_title="Product Line Profitability & Margin Performance Analysis",initial_sidebar_state="expanded") 
+
+     
+st.title("Product Line Profitability & Margin Performance Analysis")
+
+with st.container(horizontal_alignment="center",vertical_alignment="center"):
+        col1,col2,col3,col4 = st.columns(4)
+        with col1:
+                df = calculate_kpis(load_data())
+                st.metric("Total Sales",np.round(df["Sales"].sum()))
+        with col2:
+                st.metric("Total Gross Profit",np.round(df["Gross Profit"].sum(),2))
+       
+        with col3:
+                st.metric("Average Gross Margin",np.round((df["Gross Profit"]/df["Sales"]).sum(),2))
+        with col4:
+                st.metric("Total Units Sold",df["Units"].sum())
+
+
+
+st.sidebar.header("Filters",)
+order_date=  st.sidebar.date_input("Enter the order date: ").strftime(r"%d/%m/%Y")
+ship_date =  st.sidebar.date_input("Enter the shipping date:  ").strftime(r"%d/%m/%Y")
+division =  st.sidebar.selectbox("Enter the division: ",["Chocolate","Sugar","Other"])
+margin=  st.sidebar.number_input("Select the margin percentage: ",0,100)
+st.header("Product Level Performance") 
+def charts(df):
+        
+        with st.container():
+                
+                col1,col2 = st.columns(2)
+                with col1:
+                        st.subheader("Top Products By Profit")
+                        products= top_products_by_profit()
+                        for i in range(1,11):
+                                st.write(f"{i}.\t{products[i-1]}")
+                with col2:
+                        st.subheader("Top Products By Margin")
+                        products= top_products_by_margin()
+                        for i in range(1,11):
+                                st.write(f"{i}.\t{products[i-1]}")
+                        
+        with st.container():
+                st.header("\n\n\n\n\nDivision Level Performance")
+                col1,col2 =st.columns([7,5])
+                with col1:
+                        fig = div_sales_chart(df)
+                        st.pyplot(fig)
+                        st.subheader("Division By Sales")
+                with col2:
+                        fig = div_margin_chart(df)
+                        st.pyplot(fig)
+                        st.subheader("Margin of Divions")
+        with st.container():
+                st.header("Cost Diagnostics")
+                col1,col2= st.columns(2)
+                with col1:
+                        fig = cost_vs_sales(df)
+                        st.pyplot(fig)
+                        st.subheader("Cost vs Sales")
+                with col2:
+                        data = margin_risk()
+                        st.table(data)
+                        st.subheader("Margin Risk Table")
+filter_chart= False
+if st.sidebar.button("Search"):
+        st.subheader("Product Table")
+        ret,df = filter_data(order_date,ship_date,division,margin)
+        if ret:
+                df = df.drop("Row ID",axis=1)
+                st.dataframe(df)
+                charts(df)
+                filter_chart = True
+        else:
+                st.error("No Record Found")
+if filter_chart == False:
+        charts(calculate_kpis(load_data()))
+
+
+with st.container():
+        st.header("Profit Concentration(Pareto) Analysis")
+        st.subheader(r"Products Contributing 80% Revenue")
+        col1,col2  = st.columns([1,2])
+       
+        with col1:
+                st.text("Product List")
+                data = revenue_pareto()
+                st.table(data["Product Name"])
+        with col2:
+                st.text("Revenue Table")
+                st.table(data)
+        st.subheader(r"Products Contributing 80% Profit")
+        col3,col4 = st.columns([1,2])
+       
+        with col3:
+                st.text("Product List")
+                data = profit_pareto()
+                st.table(data["Product Name"])
+        with col4:
+                st.text("Profit Table")
+                st.table(data)
+with st.container():
+        st.subheader("Revenue Pareto Charts")
+        col1,col2 = st.columns(2)
+        chart1,chart2 = revenue_pareto_chart()
+        with col1:
+                st.pyplot(chart1)
+                st.text("Sales vs Products")
+        with col2:
+                st.pyplot(chart2)
+                st.text("Revenue(%) vs Products")
+        st.subheader("Profit Pareto Charts")
+        col3,col4 = st.columns(2)
+        chart3,chart4 = profit_pareto_chart()
+        with col3:
+                st.pyplot(chart3)
+                st.text("Gross Profit vs Products")
+        with col4:
+                st.pyplot(chart4)
+                st.text("Profit(%) vs Products")
+
+st.header("Factory Wise Analysis")
+st.subheader("Factory Table")
+table,sales_chart,profit_chart,cost_chart,margin_chart = factory_analysis()
+st.table(table)
+st.subheader("Factory Charts")
+with st.container():
+        col1,col2 = st.columns(2)
+        with col1:
+                st.pyplot(sales_chart)
+                st.text("Factory Sales")
+        with col2:
+                st.pyplot(profit_chart)
+                st.text("Factory Profit")
+        col3,col4 = st.columns(2)
+        with col3:
+                st.pyplot(cost_chart)
+                st.text("Factory Cost")
+        with col4:
+                st.pyplot(margin_chart)
+                st.text("Factory Margin")
+                
+                
---
+++ utils.py
@@ -0,0 +1,210 @@
+
+from re import S
+
+from click import group
+import matplotlib.pyplot as plt 
+import numpy as np
+import pandas as pd
+
+
+def load_data():
+    df = pd.read_csv("Nassau Candy Distributor.csv")
+    return df
+
+def data_info():
+    df = load_data()
+    print(df._data)
+    print(df.columns)
+    print(df.shape)
+    print(type(df))
+    print(df.notna().sum())
+    print(df.describe())
+    print(df.head())
+    
+def clean_data(df):
+    #checking zero sales
+    for i in df["Sales"]:
+        if i <=0:
+            print(i)
+    else:
+        print("Found No Sale less than or equal to 0")
+
+    #finding the rows where cost is more than sales
+    count = 0
+    for i in range(10194):
+        if df["Cost"][i]  > df["Sales"][i]:
+            count+=1
+    if count == 0 :
+        print("Found no record where cose is more than sales")
+    else:
+        print(f"Found {count} records where cost is more than sales")
+
+    #checking missing values
+    if df.isnull().sum().sum() == 0:
+        print("No missing values in the dataset")
+    else:
+        df.dropna()
+
+
+
+    #checking duplicate records in dataset
+    if df.duplicated().sum() == 0:
+        print("There is no duplicate record in the dataset")
+    else:
+        df = df.drop_duplicates()
+        
+    #checking no. of divisions 
+    print(df["Division"].value_counts())
+    print(df["Product Name"].value_counts())
+    print(df["Product ID"].value_counts())
+
+def calculate_kpis(df):
+    df["Gross Margin"] = (df["Gross Profit"]/df["Sales"])*100
+    df["Profit Per Unit"] = df["Gross Margin"]/df["Units"]
+    df["Revenue Contribution"] = (df["Sales"]/df["Sales"].sum())*100
+    df["Profit Contribution"] = (df["Gross Profit"]/df["Gross Profit"].sum())*100
+    return df
+
+def filter_data(order_date=None,ship_date=None,division=None,margin=None):
+    df = calculate_kpis(load_data()) 
+    df["Order Date"] = df["Order Date"].astype(str).str.replace("-","/",regex=False)
+    df["Ship Date"] = df["Ship Date"].astype(str).str.replace("-","/",regex=False)
+  
+    df = df[(df["Order Date"] == order_date) 
+            & (df["Ship Date"] == ship_date)
+            & (df["Division"] == division )
+            |(df["Gross Margin"] == margin)]
+   
+    if not df.empty:
+        return True,df
+    else:
+        return False,None
+def top_products_by_profit():
+    df = calculate_kpis(load_data())
+    top_by_profit = dict(df.groupby("Product Name")["Profit Contribution"].sum())
+    top_10 = dict(sorted(top_by_profit.items(),key= lambda x: x[1],reverse=True)[:10])
+    return list(top_10.keys())
+
+def top_products_by_margin():
+    df = calculate_kpis(load_data())
+    product_by_margin = dict(df.groupby("Product Name")["Gross Margin"].sum())
+    top_10 = dict(sorted(product_by_margin.items(),key = lambda x: x[1],reverse=True)[:10])
+    return list(top_10.keys())    
+
+def div_sales_chart(df):
+    
+    div_df = df.groupby("Division")[["Sales","Gross Profit"]].sum()
+    div_df.plot(kind="bar",figsize=(8,5))
+    fig1 = plt.gcf()
+    plt.xlabel("Divisions")
+    plt.ylabel("Amount")
+  
+
+    return fig1
+def div_margin_chart(df):
+   
+    div_df = dict(df.groupby("Division")["Gross Margin"].sum())
+   
+    fig2,ax = plt.subplots() 
+    
+    ax.pie(list(div_df.values()),autopct = "%1.1f%%",radius=0.7,labels = list(div_df.keys()))
+
+    return fig2
+def cost_vs_sales(df):
+   
+    x = df["Cost"]
+    y = df["Sales"]
+    fig3,ax = plt.subplots()
+    ax.scatter(x,y,c="Red")
+    ax.set_xlabel("Cost")
+    ax.set_ylabel("Sales")
+    return fig3
+def margin_risk():
+    df = calculate_kpis(load_data())
+    products = dict(df.groupby("Product Name")["Gross Margin"].sum())
+    risk=[]
+    total= 0
+    for i in products.values():
+        total += i
+    for i in products.values():
+        risk.append(np.round((i/total)*100,2))
+    risk_df = pd.DataFrame({"Product":list(products.keys()),"Margin(%)":risk})
+   
+    risk_df.loc[risk_df["Margin(%)"]>10, "Status"]  = "Good"
+    risk_df.loc[risk_df["Margin(%)"]<10 ,"Status"]= "Risk"
+    return risk_df
+def revenue_pareto():
+    df = calculate_kpis(load_data())
+    revenue= df.groupby("Product Name")["Sales"].sum()
+    revenue = revenue.sort_values(ascending=False)
+    revenue = revenue.reset_index()
+    revenue["cum_revenue"]=revenue["Sales"].cumsum()
+    revenue["cum(%)"]= np.round((revenue["cum_revenue"]/revenue["Sales"].sum())*100,1)
+    return revenue.loc[:3]
+
+def profit_pareto():
+    df = calculate_kpis(load_data())
+    profit = df.groupby("Product Name")["Gross Profit"].sum()
+    profit = profit.sort_values(ascending=False)
+    profit = profit.reset_index()
+    profit["cum_profit"]= profit["Gross Profit"].cumsum()
+    profit["cum(%)"] = np.round((profit["cum_profit"]/profit["Gross Profit"].sum())*100,1)
+    return profit.loc[:3]
+def revenue_pareto_chart():
+    df= revenue_pareto()
+    x = ["Product1","Product2","Product3","Product4"]
+    y = df["Sales"]
+    fig1 ,ax = plt.subplots()
+    ax.bar(x,y)
+    y1= df["cum(%)"]
+    fig2,ax =plt.subplots()
+    ax.scatter(x,y1)
+    return fig1,fig2
+def profit_pareto_chart():
+    df= profit_pareto()
+    x = ["Product1","Product2","Product3","Product4"]
+    y = df["Gross Profit"]
+    fig1 ,ax = plt.subplots()
+    ax.bar(x,y)
+    y1= df["cum(%)"]
+    fig2,ax =plt.subplots()
+    ax.scatter(x,y1)
+    return fig1,fig2
+
+def factory_analysis():
+    df = calculate_kpis(load_data())
+    factory_map = {
+        "Wonka Bar - Nutty Crunch Surprise":"Lot's O'Nuts",
+        "Wonka Bar - Fudge Mallows":"Lot's O'Nuts",
+        "Wonka Bar - Scrumdiddlyumptious":"Lot's O'Nuts",
+        "Wonka Bar - Milk Chocolate":"Wicked Choccy's",
+        "Wonka Bar - Triple Dazzle Caramel":"Wicked Choccy's",
+        "Laffy Taffy":"Sugar Shack",
+        "SweeTARTS":"Sugar Shack",
+        "Nerds":"Sugar Shack",
+        "Fun Dip":"Sugar Shack",
+        "Fizzy Lifting Drinks":"Sugar Shack",
+        "Everlasting Gobstopper":"Secret Factory",
+        "Hair Toffee":"The Other Factory",
+        "Lickable Wallpaper": "Secret Factory",
+        "Wonka Gum":"Secret Factory",
+        "Kazookies":"The Other Factory"
+        
+    }
+    
+    df["Factory"]= df["Product Name"].map(factory_map)
+    grouped_data = df.groupby("Factory")[["Sales","Gross Profit","Cost","Gross Margin"]].sum().sort_values(by=["Sales"],ascending=False)
+    grouped = list(df["Factory"].unique())
+    grouped.pop(2)
+    grouped = pd.Series(grouped)
+    #sales chart,gross chart and margin chart and cost hart
+    fig1,ax = plt.subplots()
+    ax.bar(grouped,np.round(grouped_data["Sales"]))
+    fig2,ax = plt.subplots()
+    ax.bar(grouped,np.round(grouped_data["Gross Profit"]))
+    fig3,ax = plt.subplots()
+    ax.bar(grouped,grouped_data["Cost"])
+    fig4,ax = plt.subplots()
+    ax.bar(grouped,grouped_data["Gross Margin"])
+
+    return grouped_data,fig1,fig2,fig3,fig4
